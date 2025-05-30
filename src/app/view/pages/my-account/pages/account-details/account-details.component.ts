a12413e678f3cc2d2d2df1b23399fa142219d1e2
Hegazy02 <120028585+Hegazy02@users.noreply.github.com>
2025-05-30 14:38:41 +0300

    feat(account-details): implement page UI, service logic, and floating error/success messages (#3)
    * feat(account-details): create account details page,service,hadling loading and error status
    
    * refacor(account-details): delete account-details service and create user service and put the component logic in the component
    
    * feat:change error message to floating message with error and success types

--- src/app/view/pages/my-account/pages/account-details/account-details.component.ts
+++ src/app/view/pages/my-account/pages/account-details/account-details.component.ts
@@ -1,9 +1,14 @@
 import { Component, inject } from '@angular/core';
 import { AccountDetailsFormComponent } from './account-details-form/account-details-form.component';
-import { AccountDetailsService } from '../../services/account-details.service';
 import { AccountDetailsProfilePicComponent } from './account-details-profile-pic/account-details-profile-pic.component';
 import { NgxSpinnerModule, NgxSpinnerService } from 'ngx-spinner';
-import { ErrorMessageComponent } from '../../../../../shared/error-message/error-message.component';
+import {
+  FloatingMessageComponent,
+  MessageType,
+} from '../../../../../shared/error-message/floating-message.component';
+import { Observable } from 'rxjs/internal/Observable';
+import { FormGroup } from '@angular/forms';
+import { UserService } from '../../../../../core/services/user.service';
 
 @Component({
   selector: 'app-account-details',
@@ -12,21 +17,29 @@ import { ErrorMessageComponent } from '../../../../../shared/error-message/error
     AccountDetailsFormComponent,
     AccountDetailsProfilePicComponent,
     NgxSpinnerModule,
-    ErrorMessageComponent,
+    FloatingMessageComponent,
   ],
   templateUrl: './account-details.component.html',
   styleUrl: './account-details.component.css',
 })
 export class AccountDetailsComponent {
-  accountDetailsService = inject(AccountDetailsService);
   spinner = inject(NgxSpinnerService);
+  userService = inject(UserService);
   isError = false;
+  isSuccess = false;
+  file?: File;
+  MessageType = MessageType;
 
-  onSubmit() {
+  onImageSelected(file: File) {
+    this.file = file;
+  }
+
+  onSubmit(accountDetailsForm: FormGroup) {
     this.spinner.show();
     this.isError = false;
+    this.isSuccess = false;
 
-    const update$ = this.accountDetailsService.updateUser();
+    const update$ = this.updateUser(accountDetailsForm);
 
     if (!update$) {
       this.spinner.hide();
@@ -34,9 +47,9 @@ export class AccountDetailsComponent {
     }
 
     update$.subscribe({
-      next: () => {
+      next: (data) => {
         this.spinner.hide();
-        this.isError = false;
+        this.isSuccess = true;
       },
       error: (err) => {
         this.spinner.hide();
@@ -44,5 +57,29 @@ export class AccountDetailsComponent {
         this.isError = true;
       },
     });
+  }
+  updateUser(accountDetailsForm: FormGroup): Observable<any> | null {
+    if (accountDetailsForm.valid) {
+      const data: [string, any][] = this.splitUserData(accountDetailsForm);
+
+      return this.userService.updateUser(data);
+    } else {
+      accountDetailsForm.markAllAsTouched();
+      return null;
+    }
+  }
+
+  private splitUserData(accountDetailsForm: FormGroup) {
+    const data: [string, any][] = [
+      ['firstName', accountDetailsForm.controls['firstName'].value],
+      ['lastName', accountDetailsForm.controls['lastName'].value],
+      ['gender', accountDetailsForm.controls['gender'].value],
+      ['phoneNumber', accountDetailsForm.controls['phoneNumber'].value],
+      ['city', accountDetailsForm.controls['city'].value],
+      ['street', accountDetailsForm.controls['street'].value],
+      ['address', accountDetailsForm.controls['address'].value],
+    ];
+    this.file && data.push(['image', this.file]);
+    return data;
   }
 }
