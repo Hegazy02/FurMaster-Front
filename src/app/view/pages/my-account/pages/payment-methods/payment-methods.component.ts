c4bc9e31db3bbdb02c59a19a396cde0b9c11342b
Hegazy <abdelrhmanhegazy02@gmail.com>
2025-05-30 17:41:11 +0300

    feat(payment-methods): create error component, creating status class to handle status in requests and handling get payment-methods request status

--- src/app/view/pages/my-account/pages/payment-methods/payment-methods.component.ts
+++ src/app/view/pages/my-account/pages/payment-methods/payment-methods.component.ts
@@ -2,20 +2,21 @@ import { Component, inject, OnInit } from '@angular/core';
 import { PaymentMethodCardComponent } from './payment-method-card/payment-method-card.component';
 import { PaymentMethod } from '../../../../../core/interfaces/payment-method.interface';
 import { PaymetMethodsService } from '../../../../../core/services/paymet-methods.service';
-import { Observable } from 'rxjs/internal/Observable';
 import { Status, StatusType } from '../../../../../core/util/status';
-import { PrimaryButtonComponent } from "../../../../../shared/primary-button/primary-button.component";
+import { PrimaryButtonComponent } from '../../../../../shared/primary-button/primary-button.component';
+import { ErrorComponent } from "../../../../../shared/error/error.component";
 
 @Component({
   selector: 'app-payment-methods',
   standalone: true,
-  imports: [PaymentMethodCardComponent, PrimaryButtonComponent],
+  imports: [PaymentMethodCardComponent, PrimaryButtonComponent, ErrorComponent],
   templateUrl: './payment-methods.component.html',
   styleUrl: './payment-methods.component.css',
 })
 export class PaymentMethodsComponent implements OnInit {
   paymetMethodsService = inject(PaymetMethodsService);
   status = new Status();
+  StatusType = StatusType;
 
   cards: PaymentMethod[] = [];
   ngOnInit(): void {
