341752a2c27738c4bb44301bbc8dc7e21254d146
Hegazy <abdelrhmanhegazy02@gmail.com>
2025-06-20 00:00:41 +0300

    refacor: refactor account details

--- src/app/view/pages/user/my-account/pages/account-details/account-details.component.ts
+++ src/app/view/pages/user/my-account/pages/account-details/account-details.component.ts
@@ -1,66 +1,43 @@
 import { Component, inject } from '@angular/core';
 import { AccountDetailsFormComponent } from './account-details-form/account-details-form.component';
 import { AccountDetailsProfilePicComponent } from './account-details-profile-pic/account-details-profile-pic.component';
-import { NgxSpinnerModule, NgxSpinnerService } from 'ngx-spinner';
-import {
-  FloatingMessageComponent,
-  MessageType,
-} from '../../../../../../shared/floating-message/floating-message.component';
 import { Observable } from 'rxjs/internal/Observable';
 import { FormGroup } from '@angular/forms';
 import { UserService } from '../../../../../../core/services/user.service';
+import { ToastrService } from 'ngx-toastr';
+import { User } from '../../../../../../core/interfaces/user.interface';
+import { AuthService } from '../../../../../../core/services/auth.service';
 
 @Component({
   selector: 'app-account-details',
   standalone: true,
-  imports: [
-    AccountDetailsFormComponent,
-    AccountDetailsProfilePicComponent,
-    NgxSpinnerModule,
-    FloatingMessageComponent,
-  ],
+  imports: [AccountDetailsFormComponent, AccountDetailsProfilePicComponent],
   templateUrl: './account-details.component.html',
   styleUrl: './account-details.component.css',
 })
 export class AccountDetailsComponent {
-  spinner = inject(NgxSpinnerService);
+  toastr = inject(ToastrService);
   userService = inject(UserService);
-  isError = false;
-  isSuccess = false;
+  authService = inject(AuthService);
   file?: File;
-  MessageType = MessageType;
 
   onImageSelected(file: File) {
     this.file = file;
   }
 
   onSubmit(accountDetailsForm: FormGroup) {
-    console.log("acac", accountDetailsForm);
-    
-    this.spinner.show();
-    this.isError = false;
-    this.isSuccess = false;
-
-    const update$ = this.updateUser(accountDetailsForm);
-
-    if (!update$) {
-      this.spinner.hide();
-      return;
-    }
-
-    update$.subscribe({
+    this.updateUser(accountDetailsForm)?.subscribe({
       next: (data) => {
-        this.spinner.hide();
-        this.isSuccess = true;
+        console.log('data', data);
+        this.authService.user = data;
+        this.toastr.success('User updated successfully.');
       },
       error: (err) => {
-        this.spinner.hide();
-        console.error('Error updating user:', err);
-        this.isError = true;
+        this.toastr.error('Could not update user.');
       },
     });
   }
-  updateUser(accountDetailsForm: FormGroup): Observable<any> | null {
+  updateUser(accountDetailsForm: FormGroup): Observable<User> | null {
     if (accountDetailsForm.valid) {
       const data: [string, any][] = this.splitUserData(accountDetailsForm);
 
